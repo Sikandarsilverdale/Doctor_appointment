4721b3ebe8e6ea8d83e16eb1bc9b9260333ebe86
Sikandarsilverdale <sikandar.hanif@silverdaletech.com>
2021-12-10 00:24:22 +0500

    [add project on branch]

---
+++ project_task_validation/__manifest__.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Project task validation",
+
+    'summary': """
+        This is a generic module and in this module we will handle all the project for
+        user so they can only start timer on one task
+        """,
+
+    'description': """
+        Long description of module's purpose
+    """,
+
+    'author': "Silverdale",
+    'website': "https://www.silverdaletech.com",
+
+
+    'category': 'Uncategorized',
+    'version': '14.0.0',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base', 'project'],
+
+    # always loaded
+    'data': [
+
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+
+    ],
+
+    'installable': True,
+    'application': True,
+
+}
---
+++ project_task_validation/models/project_task.py
@@ -0,0 +1,17 @@
+from odoo import models, fields, api
+from odoo.exceptions import UserError
+
+
+class projectTask(models.Model):
+    _inherit = 'project.task'
+
+    def action_timer_start(self):
+        """ check the other running project task if already running raise
+        an error timesheet already in progress with task name and id
+        otherwise create a task and start timer
+               """
+        other_running_tasks = self.search([('user_timer_id', '!=', False), ('user_timer_id.user_id', '=', self.env.user.id), ('id', '!=', self.id)])
+        if other_running_tasks and any([x.is_timer_running for x in other_running_tasks.filtered(lambda t: t.user_timer_id).mapped('user_timer_id')]):
+            name = other_running_tasks.name_get()
+            raise UserError(f'Timesheet already in progress: {name}')
+        super(projectTask, self).action_timer_start()
